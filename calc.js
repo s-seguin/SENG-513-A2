c14b2e2e3c413ada44cf3f2c0ec6fde5c6078c46
s-seguin <stuart.seguin@ucalgary.ca>
2019-02-07 00:31:14 -0700

    added some more logic - not all working

--- calc.js
+++ calc.js
@@ -1,22 +1,36 @@
 let resetDisplayFlag = false;
 
 function btnClick(btn) {
-    console.log("reset: ", resetDisplayFlag);
+   // console.log("reset: ", resetDisplayFlag);
     if (resetDisplayFlag)
         clearDisplay();
-    console.log("User clicked " + btn);
-    document.getElementById("display").innerHTML += btn;
+    //console.log("User clicked " + btn);
+    //this isnt working for operators
+    if ((btn != '*' || btn != '/' || btn != '+' || btn != '-') && eval(document.getElementById("display").innerText) === 0)
+        document.getElementById("display").innerText = btn;
+    else
+        document.getElementById("display").innerText += btn;
 }
 
 function evalExpression() {
     let currentExpression = document.getElementById("display").innerText;
     let ex = eval(currentExpression);
     console.log(ex);
-    document.getElementById("display").innerHTML = ex;
+    console.log(currentExpression);
+    document.getElementById("prevCalcDisplay").innerText = currentExpression + " =";
+    document.getElementById("display").innerText = ex;
     resetDisplayFlag = true;
 }
 
 function clearDisplay() {
-    document.getElementById("display").innerHTML = "";
+    document.getElementById("display").innerText = "";
     resetDisplayFlag = false;
+}
+
+function deleteLastEntry() {
+    let entry = document.getElementById("display").innerText;
+    if (entry.length == 1)
+        document.getElementById("display").innerText = "0";
+    else
+        document.getElementById("display").innerText = entry.slice(0, entry.length -1);
 }
